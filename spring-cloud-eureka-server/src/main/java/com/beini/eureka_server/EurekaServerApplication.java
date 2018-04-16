c411c864cad1790415f9910fb488f16357738b53
Administrator <Administrator@lb_chen>
2018-04-16 19:35:14 +0800

    update project structor

--- spring-cloud-eureka-server/src/main/java/com/beini/eureka_server/EurekaServerApplication.java
+++ spring-cloud-eureka-server/src/main/java/com/beini/eureka_server/EurekaServerApplication.java
@@ -1,13 +1,13 @@
-package com.beini.springcloudeureka;
+package com.beini.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudEurekaApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
--- spring-cloud-eureka-server/src/test/java/com/beini/eureka_server/SpringCloudEurekaApplicationTests.java
+++ spring-cloud-eureka-server/src/test/java/com/beini/eureka_server/SpringCloudEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.beini.springcloudeureka;
+package com.beini.eureka_server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
