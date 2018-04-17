cf738a5a47df3a7e34c089e0f0e7e116db13a2ec
Administrator <Administrator@lb_chen>
2018-04-17 09:59:09 +0800

    update the simple config

--- spring-cloud-eureka-server/src/main/java/com/beini/eureka_server/EurekaServerApplication.java
+++ spring-cloud-eureka-server/src/main/java/com/beini/eureka_server/EurekaServerApplication.java
@@ -3,6 +3,7 @@ package com.beini.eureka_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
